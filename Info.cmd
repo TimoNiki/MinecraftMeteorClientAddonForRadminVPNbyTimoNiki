b11be31c0ba7c7d4524e63072cd53c6d84d99059
LightLight01 <300591238+LightLight01@users.noreply.github.com>
2026-07-17 19:31:06 +0300

    Указал копирайт TimoNiki его же аддон а я помог ему с аддоном

--- Info.cmd
+++ Info.cmd
@@ -3,3 +3,4 @@ echo Please on compile run JVM 21.
 echo Addon by TimoNiki and LightLight01.
 echo if errors save the Minecraft log and say to support.
 echo support in testing
+echo Copyright ©️ 2026 TimoNiki
